fa3f59749ee3544b0dd650af4821db33cf44a834
xinghaowen <1924533433@qq.com>
2021-02-03 14:32:41 +0800

    feat:mypanel

--- src/main/java/com/thief/yuki/Mypanel.java
+++ src/main/java/com/thief/yuki/Mypanel.java
@@ -8,46 +8,53 @@ import java.awt.image.*;
  * @author by xinghaowen
  * @version 1.0
  * @classname Mypanel
- *
  */
 public class Mypanel extends JPanel {
+    private final int xMax;
+    private final int yMax;
     private Image timg;
-    private  boolean isTransparent;
+    private boolean isTransparent;
     private int filterRgb;
-    Mypanel(Image image,boolean isTransparent,int filterRgb) {
+
+    Mypanel(Image image, boolean isTransparent, int filterRgb) {
         this.timg = image;
         this.isTransparent = isTransparent;
         this.filterRgb = filterRgb;
-//        this.xMax=timg.getWidth((img, infoflags, x, y, width, height) -> false)/2;
-//        this.yMax=timg.getHeight((img, infoflags, x, y, width, height) -> false)/5;
+        this.xMax = timg.getWidth((img, infoflags, x, y, width, height) -> false) / 2;
+        this.yMax = timg.getHeight((img, infoflags, x, y, width, height) -> false) / 5;
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         Image im;
-        if (isTransparent){
-            ImageFilter imgf = new MyFilter(255,filterRgb);
+        if (isTransparent) {
+            ImageFilter imgf = new MyFilter(255, filterRgb, xMax, yMax);
             FilteredImageSource fis = new FilteredImageSource(timg.getSource(), imgf);
             im = Toolkit.getDefaultToolkit().createImage(fis);
-        }else {
-            im=timg;
+        } else {
+            im = timg;
         }
         Graphics2D g1 = (Graphics2D) g;
         g1.drawImage(im, 0, 0, this);
     }
+
     static class MyFilter extends RGBImageFilter {// 抽象类RGBImageFilter是ImageFilter的子类，
         // 继承它实现图象ARGB的处理
         int alpha = 0;
         private int filterRgb;
+        private final int xMax;
+        private final int yMax;
 
-        public MyFilter(int alpha,int filterRgb) {// 构造器，用来接收需要过滤图象的尺寸，以及透明度
+        public MyFilter(int alpha, int filterRgb, int xMax, int yMax) {// 构造器，用来接收需要过滤图象的尺寸，以及透明度
             this.canFilterIndexColorModel = true;
             this.filterRgb = filterRgb;
             // TransparentImageFilter类继承自RGBImageFilter，它的构造函数要求传入原始图象的宽度和高度。
             // 该类实现了filterRGB抽象函数
             // ，缺省的方式下，该函数将x，y所标识的象素的ARGB值传入，程序员按照一定的程序逻辑处理后返回该象素新的ARGB值
             this.alpha = alpha;
+            this.xMax = xMax;
+            this.yMax = yMax;
         }
 
         @Override
@@ -61,8 +68,7 @@ public class Mypanel extends JPanel {
             int redFilter = dcm.getRed(filterRgb);
             int greenFilter = dcm.getGreen(filterRgb);
             int blueFilter = dcm.getBlue(filterRgb);
-//            (x>xMax&&y<yMax&&(red > 240 && green >90&& blue >160);
-            if ((red == redFilter && blue == blueFilter && green == greenFilter)) {//
+            if ((red == redFilter && blue == blueFilter && green == greenFilter) || (x > xMax && y < yMax && (red > 240 && green > 90 && blue > 160))) {//
                 alpha = 0;
             } else {
                 alpha = 255;
